89111ef45b9fde5aa85def7d7a5932f2edaef80f
PDF Triangle User <>
2026-08-06 12:22:30 +0300

    chore: remove ai-video-saas from this repo

--- ai-video-saas/src/modules/marketpilot/social-publisher/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./types/publisher.types";
-export * from "./contentFormatter";
-export * from "./platformAdapters";
-export * from "./analyticsTracker";
-export * from "./campaignManager";
-export * from "./scheduler";
-export * from "./publisherEngine";
--- ai-video-saas/src/modules/marketpilot/video-generator/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./types/generator.types";
-export * from "./assetGenerator";
-export * from "./voiceGenerator";
-export * from "./sceneRenderer";
-export * from "./timelineBuilder";
-export * from "./renderCoordinator";
-export * from "./videoExporter";
